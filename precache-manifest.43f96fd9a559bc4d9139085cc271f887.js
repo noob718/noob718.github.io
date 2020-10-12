0cb380760af1fb00e0cadf3aac2e4a4ac1b7a09b
jason sutherland <zoidfirz@gmail.com>
2020-10-12 18:40:30 -0400

    first commit

---
+++ precache-manifest.43f96fd9a559bc4d9139085cc271f887.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d16d318fbcae4ea87856",
+    "url": "css/about.b8d18798.css"
+  },
+  {
+    "revision": "20972b3b0a871cee547f",
+    "url": "css/app.f381fb1a.css"
+  },
+  {
+    "revision": "bf24df3f0dc2ef52452651116f66f543",
+    "url": "img/dashboard.bf24df3f.png"
+  },
+  {
+    "revision": "ea9f52dae1e06edc73fe01d15175cc8d",
+    "url": "img/ekg2.ea9f52da.jpg"
+  },
+  {
+    "revision": "8c9e13dd5960111bd0795120e1f5bac1",
+    "url": "img/games2.8c9e13dd.png"
+  },
+  {
+    "revision": "531d96d1d660864b266c37bbabf969d4",
+    "url": "img/header.531d96d1.jpg"
+  },
+  {
+    "revision": "5dde9ef47af2c777a7cdbdd29102bcbb",
+    "url": "img/heart.5dde9ef4.png"
+  },
+  {
+    "revision": "45edcbbe0593592e4fee00423ed7a3cd",
+    "url": "img/house.45edcbbe.png"
+  },
+  {
+    "revision": "165d18eb4a6a46331c9ef1ea990c71ca",
+    "url": "img/logo-new.165d18eb.png"
+  },
+  {
+    "revision": "f0e0263b6313b95099930ae65befdd92",
+    "url": "img/logout.f0e0263b.png"
+  },
+  {
+    "revision": "b8cfb48bd52cfa29d9e42268faa86ee5",
+    "url": "img/logout2.b8cfb48b.png"
+  },
+  {
+    "revision": "fa1c02238a438009be269a7261e37295",
+    "url": "img/star-of-life.fa1c0223.png"
+  },
+  {
+    "revision": "df6f43de08be1e0d512a93757f3586fe",
+    "url": "index.html"
+  },
+  {
+    "revision": "d16d318fbcae4ea87856",
+    "url": "js/about.37da3665.js"
+  },
+  {
+    "revision": "20972b3b0a871cee547f",
+    "url": "js/app.29f4ff1e.js"
+  },
+  {
+    "revision": "2d8448df59d47dc665cd",
+    "url": "js/chunk-vendors.231a3761.js"
+  },
+  {
+    "revision": "6bc0f11d6d7600602292a06660b75f72",
+    "url": "manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "robots.txt"
+  }
+]);
