199d32d2ae7213f0d70401bda98e4e8f35a5256d
zoidfirz <zoidfirz@gmail.com>
2020-12-27 02:12:54 -0500

    deploy

---
+++ precache-manifest.3f5b7e9f2524dca3b3222e48cb1f98b6.js
@@ -0,0 +1,210 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b0c093159b36d80ae1950c5d8c1c59dd",
+    "url": "_redirects"
+  },
+  {
+    "revision": "cbebadf97af703b75cfb",
+    "url": "css/about.c17f9ae7.css"
+  },
+  {
+    "revision": "11ceacd8e53797e07c51",
+    "url": "css/app.3c02a032.css"
+  },
+  {
+    "revision": "28f149ff6f0815923c007ebc410ac053",
+    "url": "img/12-lead.28f149ff.jpg"
+  },
+  {
+    "revision": "d9180dccfae483ac6f5851fa944e3644",
+    "url": "img/bulb.d9180dcc.png"
+  },
+  {
+    "revision": "f5328dfe57a33b34e2eb069a9d6f3602",
+    "url": "img/check-mark.f5328dfe.png"
+  },
+  {
+    "revision": "c0cddd89cae1dd067eafb371c9fd9a16",
+    "url": "img/chest-pain.c0cddd89.png"
+  },
+  {
+    "revision": "7d2ad399e2e95afaee8562d1d0e0ffa1",
+    "url": "img/cpap-strap-1.7d2ad399.png"
+  },
+  {
+    "revision": "72b43f62004c362ecc1696dcbd5d268c",
+    "url": "img/cpap-strap-2.72b43f62.png"
+  },
+  {
+    "revision": "9116d1769e791844fcb708b2c2bc3852",
+    "url": "img/cpap-strap-3.9116d176.png"
+  },
+  {
+    "revision": "62bf5e00b6d7286737df463b6bad98b5",
+    "url": "img/cpap.62bf5e00.png"
+  },
+  {
+    "revision": "bf24df3f0dc2ef52452651116f66f543",
+    "url": "img/dashboard.bf24df3f.png"
+  },
+  {
+    "revision": "a24201811b5a52422c2376ab3d33ee46",
+    "url": "img/emt-patch.a2420181.png"
+  },
+  {
+    "revision": "5f5807eae75fa517f08be0e60adafbad",
+    "url": "img/feedback-img.5f5807ea.png"
+  },
+  {
+    "revision": "94309c1287747896b0eb5382ecce2938",
+    "url": "img/flag.94309c12.png"
+  },
+  {
+    "revision": "a3faa8c38f287dcec42855e4c41702ec",
+    "url": "img/gear-orange.a3faa8c3.png"
+  },
+  {
+    "revision": "c2e2aca62073034f43ac652b927ad178",
+    "url": "img/gear.c2e2aca6.png"
+  },
+  {
+    "revision": "880c4c99ffb504b6cdf8840a640286bf",
+    "url": "img/hazard.880c4c99.png"
+  },
+  {
+    "revision": "531d96d1d660864b266c37bbabf969d4",
+    "url": "img/header.531d96d1.jpg"
+  },
+  {
+    "revision": "7e537743bc8c8fd095d42f3186190860",
+    "url": "img/heart-ekg.7e537743.png"
+  },
+  {
+    "revision": "3724e56b8803c776387fe360a1e3f7bf",
+    "url": "img/heart-mod.3724e56b.png"
+  },
+  {
+    "revision": "eddfb90e01c3020557c7f5ec7655d4e7",
+    "url": "img/heart.eddfb90e.png"
+  },
+  {
+    "revision": "45edcbbe0593592e4fee00423ed7a3cd",
+    "url": "img/house.45edcbbe.png"
+  },
+  {
+    "revision": "b0dd35e51f10ba70a6dcfd1b491ea028",
+    "url": "img/io.b0dd35e5.png"
+  },
+  {
+    "revision": "fdb5badc01007167e021d569e3af237d",
+    "url": "img/iv.fdb5badc.png"
+  },
+  {
+    "revision": "506f65a048b5e3b3603ca75ea4abac6a",
+    "url": "img/just-logo.506f65a0.png"
+  },
+  {
+    "revision": "ce6b56677ab7e29094c53b5472832614",
+    "url": "img/logo-final.ce6b5667.png"
+  },
+  {
+    "revision": "f0e0263b6313b95099930ae65befdd92",
+    "url": "img/logout.f0e0263b.png"
+  },
+  {
+    "revision": "b8cfb48bd52cfa29d9e42268faa86ee5",
+    "url": "img/logout2.b8cfb48b.png"
+  },
+  {
+    "revision": "a90093ba54491d134e7d7face6755406",
+    "url": "img/lungs.a90093ba.png"
+  },
+  {
+    "revision": "d45fb59c2f02808e10e21b211cc98649",
+    "url": "img/math.d45fb59c.png"
+  },
+  {
+    "revision": "05f1f18319847a9710c00f3dfaec6940",
+    "url": "img/medic-patch.05f1f183.png"
+  },
+  {
+    "revision": "7f21eeaf72fcdf08bc435e5a26b8da4f",
+    "url": "img/password-hidden.7f21eeaf.png"
+  },
+  {
+    "revision": "6b413a146640e20a1836ea10b1b0830b",
+    "url": "img/password-visible.6b413a14.png"
+  },
+  {
+    "revision": "276402cdd2dd74c63b28c6e241064aa5",
+    "url": "img/physical.276402cd.png"
+  },
+  {
+    "revision": "f45636596e94f3f4b98533f4ba1247f1",
+    "url": "img/pill.f4563659.png"
+  },
+  {
+    "revision": "6a36cdeb2fd2408477696bb544feb92a",
+    "url": "img/procedure.6a36cdeb.png"
+  },
+  {
+    "revision": "ef97162a7e9b34273d9d5af0458a6f62",
+    "url": "img/progress-img.ef97162a.png"
+  },
+  {
+    "revision": "5c9711f74e71c1d0ca7bdc851a808acf",
+    "url": "img/radio.5c9711f7.png"
+  },
+  {
+    "revision": "ed78eb4d0a9b02983bf947170c0cd953",
+    "url": "img/restart.ed78eb4d.png"
+  },
+  {
+    "revision": "0f1575c6555a077ca2f8c513a6658f1b",
+    "url": "img/star-of-life-mod.0f1575c6.png"
+  },
+  {
+    "revision": "139a79cf8d361ecb32b43bff25ffbe0b",
+    "url": "img/star.139a79cf.png"
+  },
+  {
+    "revision": "3065439b6615ac0635305dd50647ba5a",
+    "url": "img/telemetry-doc-2.3065439b.png"
+  },
+  {
+    "revision": "273f6756d857ca675678572083855ca4",
+    "url": "img/telemetry-phone.273f6756.png"
+  },
+  {
+    "revision": "f7fad3c5a4124cf30a7452f97d20144f",
+    "url": "img/transport.f7fad3c5.png"
+  },
+  {
+    "revision": "31490f22ff00005ea0d9ce874ab77cdf",
+    "url": "img/tubing.31490f22.png"
+  },
+  {
+    "revision": "fd6188a8e2a66ab72b23d2e7ee8325ec",
+    "url": "index.html"
+  },
+  {
+    "revision": "cbebadf97af703b75cfb",
+    "url": "js/about.d9069933.js"
+  },
+  {
+    "revision": "11ceacd8e53797e07c51",
+    "url": "js/app.4a860a29.js"
+  },
+  {
+    "revision": "c0c445891fbd46a6dbec",
+    "url": "js/chunk-vendors.db90d6ae.js"
+  },
+  {
+    "revision": "6bc0f11d6d7600602292a06660b75f72",
+    "url": "manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "robots.txt"
+  }
+]);
